aeca3a908ec4f2a2e30ceeb4df89c2d5396ce03c
zxyadc <101701125+zxyadc@users.noreply.github.com>
2024-01-23 01:59:41 +0800

    Create URL.java

---
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/URL.java
@@ -0,0 +1,25 @@
+package com.github.tvbox.osc.ui.activity;
+
+public class URL {
+    public static String DOMAIN_NAME_PROXY = "";
+    public static final String GITHUB_VERSION_PATH = ""; // 更新说明
+    public static final String APK_PATH = "";    // 下载地址
+ 
+    // public static final String DOMAIN_NAME_PROXY = "http://192.168.3.159:81";
+    // public static final String GITHUB_VERSION_PATH = "/chfs/shared/Github_Project/box/version.json";
+    // public static final String APK_PATH = "/chfs/shared/Github_Project/box/Release/app-release.apk";
+    // https://raw.staticdn.net/mlabalabala/TVResource/main/boxCfg/ori_source.json
+    // https://raw.gitmirror.com/mlabalabala/TVResource/main/boxCfg/ori_source.json
+    // https://ghproxy.com/https://raw.githubusercontent.com//mlabalabala/TVResource/main/boxCfg/ori_source.json
+    // https://raw.bunnylblbblbl.eu.org/https://raw.githubusercontent.com/mlabalabala/TVResource/main/boxCfg/ori_source.json
+    // 默认线路
+    public static final String DEFAULT_API_NAME = "默认";
+    public static final String DEFAULT_API_URL = "https://raw.gitmirror.com/gaotianliuyun/gao/master/0825.json";
+    // public static final String DEFAULT_API_URL = "https://raw.githubusercontent.com/gaotianliuyun/gao/master/0821.json";
+    // 默认仓库线路
+    public static final String DEFAULT_STORE_API_URL = "https://zhxy.neocities.org/duo1.json";
+    // 默认直播线路
+    public static final String DEFAULT_LIVE_URL = "https://raw.gitmirror.com/mlabalabala/TVResource/main/boxCfg/live.txt";
+
+
+}
